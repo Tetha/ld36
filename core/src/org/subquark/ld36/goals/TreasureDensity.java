2945e53c52e53819b02b56bb9d77c4623ba02fa2
Harald Krämer <tetha42@gmail.com>
2016-08-28 10:50:13 +0200

    Adjusted treasure density now that the F3 map is working

--- core/src/org/subquark/ld36/goals/TreasureDensity.java
+++ core/src/org/subquark/ld36/goals/TreasureDensity.java
@@ -3,7 +3,7 @@ package org.subquark.ld36.goals;
 public enum TreasureDensity {
     LOTS(0.1f, 2, 0.75f),
     SOME(0.3f, 4, 0.8f),
-    NONE(0.8f, 5, 0.85f);
+    NONE(0.5f, 4, 0.85f);
     
     public final float persistence;
     public final int octaves;
@@ -13,6 +13,5 @@ public enum TreasureDensity {
         this.persistence = persistence;
         this.octaves = octaves;
         this.treasureThreshold = treasureThreshold;
-    }
-    
+    }    
 }
