078f55d3af18c8a8583f3ca0f11fb8ab37b502b8
Harald Krämer <tetha42@gmail.com>
2016-08-28 19:28:53 +0200

    very lazy logo

--- core/src/org/subquark/ld36/menu/MenuDisplay.java
+++ core/src/org/subquark/ld36/menu/MenuDisplay.java
@@ -67,43 +67,5 @@ public class MenuDisplay {
         
         batch.draw(textures.playButton, 270, 60);
         batch.end();
-        /*
-        lines.begin(ShapeType.Line);
-        lines.rect(130 + 10, 210 + 10, 90, 40);
-        lines.rect(230 + 10, 210 + 10, 90, 40);
-        lines.rect(330 + 10, 210 + 10, 90, 40);
-
-        lines.rect(130 + 10, 260 + 10, 90, 40);
-        lines.rect(230 + 10, 260 + 10, 90, 40);
-        lines.rect(330 + 10, 260 + 10, 90, 40);
-
-        lines.rect(130 + 10, 310 + 10, 90, 40);
-        lines.rect(230 + 10, 310 + 10, 90, 40);
-        lines.rect(330 + 10, 310 + 10, 90, 40);
-        lines.end();
-        
-        batch.begin();
-        font.draw(batch, "Egypt Excavation", 0, 450);
-        
-        font.draw(batch, "Much Time", 150, 400);
-        font.draw(batch, "Some Time", 250, 400);
-        font.draw(batch, "Little Time", 350, 400);
-        
-        font.draw(batch, "Lots of Treasure", 0, 350);
-        font.draw(batch, "Some Treasure", 0, 300);
-        font.draw(batch, "Almost no Treasure", 0, 250);
-        
-        font.draw(batch, "Dig!", 170, 250);
-        font.draw(batch, "Dig!", 270, 250);
-        font.draw(batch, "Dig!", 370, 250);
-        
-        font.draw(batch, "Dig!", 170, 300);
-        font.draw(batch, "Dig!", 270, 300);
-        font.draw(batch, "Dig!", 370, 300);
-        
-        font.draw(batch, "Dig!", 170, 350);
-        font.draw(batch, "Dig!", 270, 350);
-        font.draw(batch, "Dig!", 370, 350);
-        batch.end();*/
     }
 }
